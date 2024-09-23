8164bf377e3cd1467071aff1ad7df35fa5472118
viniciusoramos <161609090+viniciusoramos@users.noreply.github.com>
2024-09-23 08:29:18 -0300

    Delete codigo/LAB_JavaParking directory

--- codigo/LAB_JavaParking/Cobranca.java
+++
@@ -1,9 +0,0 @@
-public class Cobranca {
-    private double valorPorMinuto = 0.266; // R$4,00 por 15 minutos
-    private double valorMaximo = 50.00;
-
-    public double calcularValor(int tempoOcupacao) {
-        double valor = tempoOcupacao * valorPorMinuto;
-        return Math.min(valor, valorMaximo);
-    }
-}
